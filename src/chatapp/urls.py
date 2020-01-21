3d9da6b4c9c7b47ef4c5c6f73c949aac6ac01042
Khaled Hamed <khaled.hamedt@gmail.com>
2020-01-21 22:21:17 +0200

    create initial chat app

--- src/chatapp/urls.py
+++ src/chatapp/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from django.conf.urls import include
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('chat/', include('chat.urls', namespace='chat')),
 ]
